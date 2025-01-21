381e31ef1a1a3c9ad2ce7ae96cf6ddc7c17f742a
raidenwrld <150138064+raidenwrld@users.noreply.github.com>
2025-01-21 16:20:39 +0000

    Menu Button functionality added

--- Assets/Scripts/UI/MenuBehaviour.cs
+++ Assets/Scripts/UI/MenuBehaviour.cs
@@ -8,24 +8,30 @@ public class MenuBehaviour : MonoBehaviour
 {
     public GameObject uiElement;
 
-    
+
     public void SwitchScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
-    
     public void EnableUIElement()
     {
         if (uiElement != null)
         {
             uiElement.SetActive(true);
         }
+    }
+    public void DisableUIElement()
+    {
+        if (uiElement != null)
+        {
+            Debug.Log("Deactivating UI element.");
+            uiElement.SetActive(false); 
+        }
         else
         {
-            Debug.LogError ("NOT ASSIGNED");
+            
         }
     }
-
 
 }
